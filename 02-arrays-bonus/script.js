cb0505fad183cf37ea057df4a03b8601e3d25a9d
fvguajexiii <f.vellucci13@outlook.it>
2025-07-03 14:46:49 +0200

    Es 2.5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -42,4 +42,11 @@ if (isFabioPresent) {
 }
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+for (let i = 0; i < teachers.length; i++) {
+  teachersString += teachers[i];
+  if (i < teachers.length - 1) {
+    teachersString += ",";
+  }
+}
+console.log(teachersString);
